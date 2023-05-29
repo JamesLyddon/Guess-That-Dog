430fd253008a1d4f517c6d1fedde39902cb76ff1
JamesLyddon <jameslyddon@gmail.com>
2023-05-29 14:00:36 +0100

    feat: added capitalization to answers

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Game = ({ randomDog, shuffledAnswers, checkAnswer, userScore }) => {
 	return (
 		<div className='rounded-xl p-4 bg-purple-900 m-2'>
@@ -13,15 +11,20 @@ const Game = ({ randomDog, shuffledAnswers, checkAnswer, userScore }) => {
 			</div>
 			<div className=' grid justify-items-center gap-2 my-4 min-w-300'>
 				{shuffledAnswers &&
-					shuffledAnswers.map((answer) => (
-						<button
-							className='bg-purple-800 rounded-full w-4/5 p-2 text-slate-300 font-semibold'
-							key={answer}
-							onClick={() => checkAnswer(answer)}
-						>
-							{answer}
-						</button>
-					))}
+					shuffledAnswers.map((answer) => {
+						const words = answer.split(' ')
+						const formattedAnswer = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+
+						return (
+							<button
+								className='bg-purple-800 rounded-full w-4/5 p-2 text-slate-300 font-semibold'
+								key={answer}
+								onClick={() => checkAnswer(answer)}
+							>
+								{formattedAnswer}
+							</button>
+						)
+					})}
 			</div>
 			{/* <h2>{userScore}</h2> */}
 		</div>
