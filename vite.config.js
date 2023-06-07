53712a6161f86124f9031288d3cfc28aa93fc162
JamesLyddon <jameslyddon@gmail.com>
2023-06-07 15:34:26 +0100

    fix: altered rollup

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: 'react-particles'
+      external: ['react-particles', 'tsparticles']
     }
   },
 })
